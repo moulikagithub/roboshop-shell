eff474e6b73bdfd92a5be7ce513ee3d305fdeb9a
User <kalidindi.moulika@gmail.com>
2023-12-15 21:46:45 -0600

    roboshop testing

---
+++ user.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGDB_HOST=mongodb.kalidindi.cloud
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE "$?" "Disabling nodejs" 
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE "$?" "enabling nodejs" 
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE "$?" "installing nodejs" 
+ID roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE "$?" "roboshop user added"
+else
+    echo -e "$Y roboshop user already added $n"
+fi    
+mkdir -p /app &>> $LOGFILE
+VALIDATE "$?" "creating app directory"
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
+VALIDATE "$?" "Downloading roboshop zip file"
+cd /app &>> $LOGFILE
+VALIDATE "$?" "changing to app"
+unzip /tmp/user.zip &>> $LOGFILE
+VALIDATE "$?" "unzipping user.zip"
+npm install &>> $LOGFILE
+VALIDATE "$?" "installing nodejs packages"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>> $LOGFILE
+VALIDATE "$?" "coping user.service"
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE "$?" "daemon reload user"
+systemctl enable user &>> $LOGFILE
+VALIDATE "$?" "enabling user" 
+systemctl start user &>> $LOGFILE
+VALIDATE "$?" "starting user"
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE "$?" "installing mongodb client"
+mongo --host mongodb.kalidindi.cloud </app/schema/user.js &>> $LOGFILE
