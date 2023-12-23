1105370ac169d56cd529e97b6fdb7b9d8fe379fc
User <kalidindi.moulika@gmail.com>
2023-12-23 16:50:07 -0600

    shell

---
+++ roboshop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG-ID=sg-04d13ac4a668fd74f
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+for i in "${INSTANCES[@]}"
+do
+   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+   then
+       INSTANCE_TYPE="t3.small"
+   else
+       INSTANCE_TYPE="t2.micro"
+   fi
+
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04d13ac4a668fd74f
+done
